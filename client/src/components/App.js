ab66abdf62ebb68267dcef9c26fc9bf96405316b
Piyush Pawar <piyushpawar25@gmail.com>
2018-08-28 01:41:26 +0530

    Added Ant Design

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from '../store';
+import { Button } from 'antd';
 
 class App extends React.Component {
 	render() {
 		return (
 			<Provider store={store}>
-				<Router />
+				<Button type="primary">Primary</Button>
 			</Provider>
 		);
 	}
