2a1bda9ad99f73821be7405a2f00ff7116f56bd2
Piyush Pawar <piyushpawar25@gmail.com>
2018-08-27 12:10:28 +0530

    Removed Ant Design

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,17 +3,11 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from '../store';
 
-import Navbar from './Navbar';
-
 class App extends React.Component {
 	render() {
 		return (
 			<Provider store={store}>
-				<Router>
-					<div className="app">
-						<Navbar />
-					</div>
-				</Router>
+				<Router />
 			</Provider>
 		);
 	}
