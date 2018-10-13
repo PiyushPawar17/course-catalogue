d38c0d7da948913989c1a0840a56334f9162b7ef
Piyush Pawar <piyushpawar25@gmail.com>
2018-10-13 20:30:12 +0530

    Ready for deployment

--- client/src/store.js
+++ client/src/store.js
@@ -5,13 +5,6 @@ import rootReducer from './reducers';
 const initialState = {};
 const middleware = [thunk];
 
-const store = createStore(
-	rootReducer,
-	initialState,
-	compose(
-		applyMiddleware(...middleware),
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-	)
-);
+const store = createStore(rootReducer, initialState, compose(applyMiddleware(...middleware)));
 
 export default store;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 const mongoose = require('./db/mongoose');
 
@@ -23,6 +24,16 @@ require('./config/passport')(passport);
 app.use('/api/users', users);
 app.use('/api/tags', tags);
 app.use('/api/tutorials', tutorials);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+	// Set a static folder
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 const port = process.env.PORT || 5000;
 
